232dcc005051fb79c511f38b44a7d28ea82871c3
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-21 07:01:17 +0100

    add custom Hook useHandlers

---
+++ src/components/templates/MainTemplate/MainT-copy.js
@@ -0,0 +1,92 @@
+import React, { useReducer } from "react";
+import FormInput from "components/organisms/FormInput/FormInput";
+import Title from "components/atoms/Title/Title";
+import TaskList from "components/molecules/TasksList/TaskList";
+
+const initialValues = {
+   inputValue: "",
+   error: "",
+   tasks: [],
+};
+
+const reducer = (state, action) => {
+   switch (action.type) {
+      case "INPUT CHANGE":
+         return {
+            ...state,
+            inputValue: action.value,
+         };
+      case "THROW ERROR":
+         return {
+            ...state,
+            error: action.text,
+         };
+      case "ADD TASKS":
+         return {
+            ...state,
+            tasks: action.add,
+         };
+      case "FILTERED TASKS":
+         return {
+            ...state,
+            tasks: action.tasksAfterDelete,
+         };
+      default:
+         return state;
+   }
+};
+
+const MainTemplate = () => {
+   const [tasksValues, dispatch] = useReducer(reducer, initialValues);
+   let { inputValue, tasks, error } = tasksValues;
+
+   const handleChangeInputValue = (e) => {
+      dispatch({
+         type: "INPUT CHANGE",
+         value: e.target.value,
+      });
+   };
+
+   const throwError = (messageError) => {
+      dispatch({ type: "THROW ERROR", text: messageError });
+   };
+
+   const clearInputValue = () => {
+      dispatch({
+         type: "INPUT CHANGE",
+         value: "",
+      });
+   };
+
+   const handleAddTask = (e) => {
+      throwError("");
+      e.preventDefault();
+      !!tasks.some((e) => e === inputValue)
+         ? throwError("Ein solcher Task existiert bereits. Task umbenennen, bitte!")
+         : !!inputValue
+         ? dispatch({
+              type: "ADD TASKS",
+              add: [...tasks, inputValue],
+           })
+         : throwError("Bitte geben Sie mindestens 2 Zeichen für Task ein!");
+
+      clearInputValue();
+   };
+
+   const handleDeleteTask = (key) => {
+      dispatch({
+         type: "FILTERED TASKS",
+         tasksAfterDelete: tasks.filter((e, i) => i !== key),
+      });
+   };
+   return (
+      <>
+         <Title />
+         <FormInput handleAddTask={handleAddTask} inputValue={inputValue} handleChangeInputValue={handleChangeInputValue} />
+         {!!error ? <p>{error}</p> : ""}
+         <TaskList tasks={tasks} deleteTask={handleDeleteTask} />
+      </>
+   );
+};
+
+export default MainTemplate;
---
+++ src/hooks/useHandlers.js
@@ -0,0 +1,87 @@
+import { useReducer } from "react";
+
+const initialValues = {
+   inputValue: "",
+   error: "",
+   tasks: [],
+};
+
+const reducer = (state, action) => {
+   switch (action.type) {
+      case "INPUT CHANGE":
+         return {
+            ...state,
+            inputValue: action.value,
+         };
+      case "THROW ERROR":
+         return {
+            ...state,
+            error: action.text,
+         };
+      case "ADD TASKS":
+         return {
+            ...state,
+            tasks: action.add,
+         };
+      case "FILTERED TASKS":
+         return {
+            ...state,
+            tasks: action.tasksAfterDelete,
+         };
+      default:
+         return state;
+   }
+};
+
+export const useHandlers = () => {
+   const [tasksValues, dispatch] = useReducer(reducer, initialValues);
+   let { inputValue, tasks, error } = tasksValues;
+
+   const handleChangeInputValue = (e) => {
+      dispatch({
+         type: "INPUT CHANGE",
+         value: e.target.value,
+      });
+   };
+
+   const throwError = (messageError) => {
+      dispatch({ type: "THROW ERROR", text: messageError });
+   };
+
+   const clearInputValue = () => {
+      dispatch({
+         type: "INPUT CHANGE",
+         value: "",
+      });
+   };
+
+   const handleAddTask = (e) => {
+      throwError("");
+      e.preventDefault();
+      !!tasks.some((e) => e === inputValue)
+         ? throwError("Ein solcher Task existiert bereits. Task umbenennen, bitte!")
+         : !!inputValue
+         ? dispatch({
+              type: "ADD TASKS",
+              add: [...tasks, inputValue],
+           })
+         : throwError("Bitte geben Sie mindestens 2 Zeichen für Task ein!");
+
+      clearInputValue();
+   };
+
+   const handleDeleteTask = (key) => {
+      dispatch({
+         type: "FILTERED TASKS",
+         tasksAfterDelete: tasks.filter((e, i) => i !== key),
+      });
+   };
+   return {
+      handleAddTask,
+      inputValue,
+      handleChangeInputValue,
+      error,
+      tasks,
+      handleDeleteTask,
+   };
+};
