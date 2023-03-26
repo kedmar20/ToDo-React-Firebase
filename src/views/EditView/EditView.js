abfc1741a4a7480dfcea7aeaa0301e1e88489e82
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-03-26 08:08:32 +0200

    update EditView

--- src/views/EditView/EditView.js
+++ src/views/EditView/EditView.js
@@ -1,29 +1,42 @@
 import React, { useEffect } from "react";
 import { useHandlers } from "hooks/useHandlers";
+import MainView from "views/MainView/MainView";
+import Input from "components/atoms/Input/Input";
+import { Button } from "components/atoms/Button/Button";
+import FormInput from "components/organisms/FormInput/FormInput";
+import TaskList from "components/molecules/TasksList/TaskList";
 
 let task;
 let key;
 let tasks;
 export const handleEdit = (e, i, t) => {
    console.log(i);
-   console.log(tasks);
    task = e;
    key = i;
    tasks = t;
 };
 
 const EditView = () => {
-   const { handleDeleteTask } = useHandlers();
+   const { handleDeleteTask, handleAddTask, tasksValues, handleChangeInputValue } = useHandlers();
 
    useEffect(() => {
       handleDeleteTask(key);
    }, []);
 
-   console.log(tasks); // tu jesteś !!! wyśweitla się PRAWIDŁOWO ILOŚĆ TASKÓW
-   //TERAZ MUSISZ TUTAJ NOWĄ FUNKCJĘ DELETUJĄCO-FILTRUJĄCĄ ZROBIĆ I WYŚWIETLIĆ INPUT I
-   //POTEM NOWĄ LISTĘ WYŚWIETLIĆ; EH KUPA ROBOTY JESZCZE
+   console.log(key);
+   console.log(tasks);
+   console.log(tasks[key]);
+   console.log(tasksValues);
 
-   return <div>{task}</div>;
+   return (
+      <>
+         <TaskList tasks={tasks}>{task}</TaskList>
+         {/* <Input onChange={handleChangeInputValue} value={tasksValues.inputValue} placeholder="schreibe hier neue Aufgabe..."></Input> */}
+         <Input value={tasks[key]} placeholder="schreibe hier neue Aufgabe..."></Input>
+         <Button type="submit">add new task</Button>
+         <FormInput></FormInput>
+      </>
+   );
 };
 
 export default EditView;
